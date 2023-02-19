723e2b0bd0ca91d83ceedc10f8af05145efefa46
LadikBeg <vladbehim@gmail.com>
2023-02-19 17:04:14 +0200

    He

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ilya botik
+          Ilya botiks
         </p>
         <a
           className="App-link"

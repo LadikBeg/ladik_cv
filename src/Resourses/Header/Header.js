54c42b767db8a0ca8356b5a8e4e397bbcdb297db
LadikBeg <vladbehim@gmail.com>
2023-02-19 17:20:00 +0200

    Header

---
+++ src/Resourses/Header/Header.js
@@ -0,0 +1,14 @@
+import '../Header/Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <p>I'm Vladyslav Behim</p>
+            <div className="navigation_menu">
+                <button className="nabigation"></button>
+            </div>
+        </div>
+    );
+}
+
+export default Header;

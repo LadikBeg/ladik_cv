2bd2f65445344c99eb6696f464fda345f95153a1
LadikBeg <vladbehim@gmail.com>
2023-02-21 21:25:22 +0200

    Header commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './Resourses/Header/Header'
+import Main from './Resourses/Main/Main'
 
 function App() {
   return (
     <div className="App">
-
       <Header/>
+        <Main/>
     </div>
   );
 }
--- src/Resourses/Header/Header.js
+++ src/Resourses/Header/Header.js
@@ -3,8 +3,7 @@ import '../Header/Header.css'
 function Header() {
     return (
         <div className="header">
-
-            <p id='main_text'>I'm Vladyslav Behim front end developer</p>
+            <p id='main_text'><b>LI Company</b></p>
             <div className="navigation_menu">
                 <button className="nabigation_button">About me</button>
                 <button className="nabigation_button">Skills</button>

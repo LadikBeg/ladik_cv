ea6e80bb52fb6b2cb2bfa8434b4b259524eb147d
LadikBeg <vladbehim@gmail.com>
2023-02-21 22:03:18 +0200

    Header commit

--- src/Resourses/Header/Header.js
+++ src/Resourses/Header/Header.js
@@ -3,11 +3,20 @@ import '../Header/Header.css'
 function Header() {
     return (
         <div className="header">
-            <p id='main_text'><b>LI Company</b></p>
+
+            <p id='main_text'><b>IV Company
+                <hr  align="center"  size="1" color="white"/>
+            </b></p>
             <div className="navigation_menu">
-                <button className="nabigation_button">About me</button>
-                <button className="nabigation_button">Skills</button>
-                <button className="nabigation_button">Contact</button>
+                <button className="nabigation_button">Services</button>
+                <button className="nabigation_button">Team</button>
+                <button className="nabigation_button">Portfolio</button>
+                <button className="nabigation_button">Skills and technologies</button>
+                <button className="nabigation_button">Contact us</button>
+            </div>
+            <div className="change_lang">
+                <p><b>Eng</b></p>
+                <p>Rus</p>
             </div>
         </div>
     );

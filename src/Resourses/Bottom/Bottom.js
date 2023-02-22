2451b48a7623dd23b91c7088b0a24d046db56978
IlyaOsw <ilya_osw@bk.ru>
2023-02-22 21:13:30 +0200

    22 vebruary commit

--- src/Resourses/Bottom/Bottom.js
+++ src/Resourses/Bottom/Bottom.js
@@ -4,6 +4,26 @@ function Bottom() {
     return (
         <div className="bottom">
 
+            <div className='footer_top'>
+                
+            </div>
+
+            {/* Тут будет линия шириной в 80-90% блока футера, отделяя верхнюю часть с инфо, от нижней*/}
+
+            <div className='footer_bottom'>
+                <div className='contact'> 
+                   <a className='contact_link' href='#'>Contact us</a>
+                </div>
+                {/* <div className='social'>
+                    <a href='#'><img>Instagram icon</img></a>
+                    <a href='#'><img>Facebook icon</img></a>
+                    <a href='#'><img>Telegram icon</img></a>
+                    </div> */}
+                <div className='scrollup'>
+                    <a className='scrollup_link' href='#'>Back to top</a>
+                </div>
+            </div>
+
         </div>
     );
 }

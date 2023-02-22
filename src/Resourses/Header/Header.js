7236ed49ea30ce1def94d4ac2d97d5d4f1797269
LadikBeg <vladbehim@gmail.com>
2023-02-22 02:11:55 +0200

    general commit

--- src/Resourses/Header/Header.js
+++ src/Resourses/Header/Header.js
@@ -8,11 +8,11 @@ function Header() {
                 <hr  align="center"  size="1" color="white"/>
             </b></p>
             <div className="navigation_menu">
-                <button className="nabigation_button">Services</button>
-                <button className="nabigation_button">Team</button>
-                <button className="nabigation_button">Portfolio</button>
-                <button className="nabigation_button">Skills and technologies</button>
-                <button className="nabigation_button">Contact us</button>
+                <button className="navigation_button">Services</button>
+                <button className="navigation_button">Team</button>
+                <button className="navigation_button">Portfolio</button>
+                <button className="navigation_button">Skills and technologies</button>
+                <button className="navigation_button">Contact us</button>
             </div>
             <div className="change_lang">
                 <p><b>Eng</b></p>
--- src/Resourses/Main/Main.js
+++ src/Resourses/Main/Main.js
@@ -1,7 +1,22 @@
+import '../Main/Main.css'
+import image from './image/uels-snowdonia-nant-peris-wales.jpg'
+
 function Main() {
     return (
         <div className="main">
-            <p> </p>
+            <div className="container">
+                <div className="left_side_content">
+                    <p id="name_of_company_lsc">IV Company</p>
+                    <p> <b>COMMUNICATION<br/>
+                        QUALITY<br/>
+                        SUPPORT</b> </p>
+                </div>
+
+                <div className="right_side_content">
+                    <p>IV company supports fulfill your web development needs. We guarantee the quality.</p>
+                </div>
+            </div>
+            <img src={image} alt="landscape" className="s"/>
         </div>
     );
 }

506077faa9106a7786b51041abd1ec34d35acfc9
IlyaOsw <ilya_osw@bk.ru>
2023-02-26 00:56:29 +0200

    git

--- src/Resourses/Bottom/Bottom.js
+++ src/Resourses/Bottom/Bottom.js
@@ -7,18 +7,11 @@ function Bottom() {
             <div className='footer_top'>
                 
             </div>
-
-            {/* Тут будет линия шириной в 80-90% блока футера, отделяя верхнюю часть с инфо, от нижней*/}
-
+            
             <div className='footer_bottom'>
-                <div className='contact'> 
-                   <a className='contact_link' href='#'>Contact us</a>
+                <div className='made_by'> 
+                   <a className='made_by_link' href='#'>Website made by IV Company</a>
                 </div>
-                {/* <div className='social'>
-                    <a href='#'><img>Instagram icon</img></a>
-                    <a href='#'><img>Facebook icon</img></a>
-                    <a href='#'><img>Telegram icon</img></a>
-                    </div> */}
                 <div className='scrollup'>
                     <a className='scrollup_link' href='#'>Back to top</a>
                 </div>

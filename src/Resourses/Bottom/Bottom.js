c6ff2bee3584b926f117d4e5088b71dd1a8db125
IlyaOsw <ilya_osw@bk.ru>
2023-02-27 19:52:40 +0200

    footer finish

--- src/Resourses/Bottom/Bottom.js
+++ src/Resourses/Bottom/Bottom.js
@@ -7,6 +7,7 @@ function Bottom() {
         <div className="bottom">
 
             <div className='footer_top'>
+
                 <div className="about_us general_style">
                     <p>{t("about_us_text")}</p>
                     <p>{t("services")}</p>
@@ -15,28 +16,35 @@ function Bottom() {
                     <p>{t("skills")}</p>
                     <p>{t("contact")}</p>
                 </div>
+
                 <div className="contact_us general_style">
-                    <p>{t("contact us")}</p>
+                    <p>{t("contact_us")}</p>
                     <p>Instagram</p>
                     <p>Facebook</p>
                     <p>Telegram</p>
                 </div>
+
                 <div className="mail_us general_style">
-                    <p>Send your email</p>
+                    <p>{t("send_your_email")}</p>
                     <input placeholder="Your e-mail"/>
                     <textarea placeholder="Your message..." ></textarea>
                     <button>Send message</button>
                 </div>
+
             </div>
 
             <hr  align="center"  size="1" color="white" width="90%"/>
+
             <div className='footer_bottom'>
+
                 <div className='made_by'> 
                    <p className='made_by_p' >Website made by IV Company</p>
                 </div>
+
                 <div className='scrollup'>
                     <p className='scrollup_p'>Back to top</p>
                 </div>
+
             </div>
 
         </div>

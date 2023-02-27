c1068ae6399e3856b918e165646e31441b1e1c55
IlyaOsw <ilya_osw@bk.ru>
2023-02-27 21:00:29 +0200

    27veb commit ilya

--- src/Resourses/Main/Main.js
+++ src/Resourses/Main/Main.js
@@ -8,6 +8,7 @@ function Main() {
     const { t } = useTranslation();
     return (
         <div className="main">
+
             <Menu/>
             <div className="container">
                 <div className="left_side_content">
@@ -21,7 +22,95 @@ function Main() {
                     <p>{t("info_about_us")}</p>
                 </div>
             </div>
+
             <img src={image} alt="landscape" className="image"/>
+
+            <div className='container_description'>
+
+                <div className='left_side_description'>
+
+                    <div className='description_title'>
+                       <p>Our services</p>
+                    </div>
+
+                    <div className='description_text'>
+                       <p>We work on projects that require strong sectoral expertise, precise communication and a resolutely contemporary approach.</p>
+                    </div>
+
+                </div>
+
+                <div className='right_side_description'>
+
+                    <div className='card'>
+
+                        <div className='card_title'>
+                            <h4>Audit & communication strategy</h4>
+                        </div>
+
+                        <div className='card_text'>
+                            <p>A solid foundation for your projects</p>
+                        </div>
+
+                    </div>
+                    <div className='card'>
+
+                        <div className='card_title'>
+                            <h4>Audit & communication strategy</h4>
+                        </div>
+
+                        <div className='card_text'>
+                            <p>A solid foundation for your projects</p>
+                        </div>
+
+                    </div>
+                    <div className='card'>
+
+                        <div className='card_title'>
+                            <h4>Audit & communication strategy</h4>
+                        </div>
+
+                        <div className='card_text'>
+                            <p>A solid foundation for your projects</p>
+                        </div>
+
+                    </div>
+                    <div className='card'>
+
+                        <div className='card_title'>
+                            <h4>Audit & communication strategy</h4>
+                        </div>
+
+                        <div className='card_text'>
+                            <p>A solid foundation for your projects</p>
+                        </div>
+
+                    </div>
+                    <div className='card'>
+
+                        <div className='card_title'>
+                            <h4>Audit & communication strategy</h4>
+                        </div>
+
+                        <div className='card_text'>
+                            <p>A solid foundation for your projects</p>
+                        </div>
+
+                    </div>
+                    <div className='card'>
+
+                        <div className='card_title'>
+                            <h4>Audit & communication strategy</h4>
+                        </div>
+
+                        <div className='card_text'>
+                            <p>A solid foundation for your projects</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
 
         </div>
     );

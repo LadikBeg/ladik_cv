7a48ad819657c48728dfb4f6d108ca412f395f3d
LadikBeg <vladbehim@gmail.com>
2023-03-01 16:34:34 +0200

    drop menu commit(fix)

--- src/Resourses/Header/scripts/scripts.js
+++ src/Resourses/Header/scripts/scripts.js
@@ -3,10 +3,10 @@
  export function showMenu(){
     let menu = document.getElementById("show_menu")
     let button = document.getElementById("button_menu")
-     if (menu.style.visibility === 'hidden'){
-         menu.style.top = '0'
-         menu.style.display = 'grid'
+     if (button.textContent === "menu"){
          menu.style.visibility = 'visible'
+         menu.style.display = 'grid'
+         menu.style.top = '0'
          button.textContent = 'close'
      }else {
          menu.style.top = '-100%'
